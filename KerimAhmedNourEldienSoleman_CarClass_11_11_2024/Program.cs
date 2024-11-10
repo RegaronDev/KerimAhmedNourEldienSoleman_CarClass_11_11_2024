39a0af2c3752db309219163091b9b52b2bd9660f
RegaronDev <68587505+RegaronDev@users.noreply.github.com>
2024-11-11 02:22:10 +0300

    Add project files.

---
+++ KerimAhmedNourEldienSoleman_CarClass_11_11_2024/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CarWorldScene;
+
+public class Car
+{
+    public float Fuel;
+    public float Speed;
+    public float Traction;
+    public string WheelType;
+
+    public float Stock = 0.75f;
+    public float Drift = 0.32f;
+    public float Grip = 1f;
+
+    public Car(float fuel, string wheelType)
+    {
+        Fuel = Math.Clamp(fuel, 0f, 100f);
+        WheelType = wheelType;
+        Traction = wheelType.ToLower() switch
+        {
+            "grippy" => Grip,
+            "slippery" => Drift,
+            _ => Stock // default traction for "stock" wheels
+        };
+        Speed = 0f;
+    }
+}
